bd71ddfab06ca858a2bbaa092f23ea19724cd125
Casey Anderson <79232791+caseande@users.noreply.github.com>
2023-04-09 16:06:07 -0700

    added dynamic trial size
    updated script  and code to run trials from 10, 100, 1000, ... etc.

--- script.bash
+++ script.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+
+
 #number of threads:
 for t in 1 4
 do
-    g++ -DNUMT=$t 0.cpp -o omptest -lm -fopenmp
-    ./omptest
+    for s in 10 100 1000 10000 100000
+    do
+        g++ -DNUMT=$t -DNUMTRIES=$s 0.cpp -o omptest -lm -fopenmp
+        ./omptest
+    done
 done
